b63f01550b2ce887ad1fb4b7cab03ac9f92ac1af
Cruise Lickerman <clickerman@ucls.uchicago.edu>
2025-01-22 07:46:33 -0600

    begin using LSP-mode

--- .emacs.d/config.el
+++ .emacs.d/config.el
@@ -105,12 +105,58 @@
  (defkey/leader
   "s" 'save-buffer
   "w" 'save-buffers-kill-emacs
-  "f" 'find-file))
+  "f" 'find-file
+  "g" 'magit
+  "b" 'switch-to-buffer))
 
 (setq package-install-upgrade-built-in t)
 (elpaca (transient :branch "main"))
-  (use-package magit :ensure t :demand t
-    :after transient)
+(use-package magit :ensure t :demand t
+  :after transient)
+
+(use-package flycheck :demand t :ensure t)
+
+(use-package lsp-mode
+  :ensure t
+  :demand t
+  :commands lsp
+  :custom
+  ;; what to use when checking on-save. "check" is default, I prefer clippy
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
+  (lsp-inlay-hint-enable t)
+  (setq lsp-inlay-hint-enable t)
+(setq lsp-headerline-breadcrumb-enable nil)
+  :config
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+
+(use-package lsp-ui
+  :ensure
+  :commands lsp-ui-mode
+  :custom
+  (lsp-ui-sideline-show-hover nil)
+  (lsp-ui-doc-show-with-moust t))
+
+(use-package company
+:ensure t
+:demand t
+:custom
+(company-idle-delay 0.5) ;; how long to wait until popup
+:bind
+(:map company-mode-map
+    ("<tab>". 'cl/tab-indent-or-complete)))
+
+(defun cl/tab-indent-or-complete ()
+ (interactive)
+ (if (minibufferp)
+     (minibuffer-complete)
+   (if (or (not yas/minor-mode)
+           (null (do-yas-expand)))
+       (if (check-expansion)
+           (company-complete-common)
+         (indent-for-tab-command)))))
+
+(use-package helm :ensure t :demand t)
 
 (use-package ivy :ensure t :demand t
   :config
@@ -129,6 +175,11 @@
 
 (setf make-backup-files nil)
 (setf kill-buffer-delete-auto-save-files t)
+
+(when (string= system-type "darwin")       
+ (setq dired-use-ls-dired nil))
+
+(setq use-package-always-ensure t)
 
 (defun cl/tangle-config () 
   (when (string-equal (buffer-file-name)
@@ -153,5 +204,13 @@
 (use-package rustic :ensure t :demand t
   :config
   (setq rustic-format-trigger 'on-save)
+  (lsp-rust-analyzer-cargo-watch-command "clippy")
+  (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
+  (lsp-rust-analyzer-display-chaining-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
+  (lsp-rust-analyzer-display-closure-return-type-hints t)
+  (lsp-rust-analyzer-display-parameter-hints nil)
+  (lsp-rust-analyzer-display-reborrow-hints nil)
   (defkey/rust
-    "r" 'rustic-compile))
+    "r" 'rustic-compile
+    "a" 'lsp-execute-code-action))
